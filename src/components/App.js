00db3c1d69d97af240d1290e4cc12b3a0599c37b
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-20 13:58:57 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [ptag, setpatg] = useState(false);
+  const handleClick = () => {
+    setpatg(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        click to show p tag
+      </button>
+      {ptag ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
